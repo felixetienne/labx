adb46398aaf41ad3cc03fd7b5f2485ed9ed5113a
felixetienne <felix.etienne.tetrault@gmail.com>
2014-10-25 16:50:56 -0400

    first commint

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+
+/*
+ * GET home page.
+ */
+
+exports.index = function(req, res){
+  res.render('index', {
+        title: 'Home',
+        text: 'lorem ipsum'
+  })
+};
+
+exports.about = function(req, res){
+     res.render('about', {
+        title: 'About',
+        text: 'lorem ipsum'
+  })
+};
