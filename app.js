d47ccb808f3d68cb8b27686de70026a31d3e4943
felixetienne <felix.etienne.tetrault@gmail.com>
2014-10-26 15:29:47 -0400

    rename app

--- app.js
+++ app.js
@@ -35,4 +35,18 @@ app.get('/contact', routes.contact);
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Node app is running at localhost: " + port)
-})
+});
+
+var pg = require('pg');
+
+app.get('/db', function (request, response) {
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM test_table', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.send(result.rows); }
+    });
+  });
+});
