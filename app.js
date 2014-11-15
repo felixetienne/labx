f0322a5605e1e1eb78562afee624124c1727f389
felixetienne <felix.etienne.tetrault@gmail.com>
2014-11-15 14:52:22 -0500

    bypass env

--- app.js
+++ app.js
@@ -8,12 +8,12 @@
 var express = require('express');
 var fs = require('fs');
 
-var envConfig = (function(filePath){
-    fs.exists(filePath, function(){
-        var env = require('node-env-file');
-        env(filePath);
-    });
-})(__dirname + '/.env');
+//var envConfig = (function(filePath){
+//    fs.exists(filePath, function(){
+//        var env = require('node-env-file');
+//        env(filePath);
+//    });
+//})(__dirname + '/.env');
 
 var app = module.exports = express.createServer();
 
