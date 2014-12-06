90389a9eba98fd6c457837338ff4aac0f3e61c21
felixetienne <felix.etienne.tetrault@gmail.com>
2014-12-06 14:47:08 -0500

    Fix JS

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,8 +1,49 @@
-window.Context = window.Context || (function(){ throw 'The "Context" module is not initialized.'; })
-window.Main = window.Main || {};
+"use strict";
 
-(function(context){
+window.main = window.main || {};
 
+window.main.dependancies = (function(dependancies){
 
+    dependancies.context = window.currentContext;
+    dependancies.jQuery = window.jQuery;
 
-})(window.Context);
+    for(var k in dependancies){
+        if(dependancies.hasOwnProperty(k) && typeof dependancies[k] === 'undefined') throw '[ERROR] The dependancy "' + k + '" is not defined.';
+    }
+
+    return dependancies;
+
+})(window.main.dependancies || {});
+
+(function(main){
+
+    var undefined = 'undefined';
+
+    main.helpers = (function(mod){
+
+        mod.isNullOrUndefined = function(obj){ return typeof obj === undefined || obj == null; }
+        mod.throw = function(exeption) { throw exeption.getMessage(); }
+
+        return mod;
+
+    })(main.helpers || {});
+
+    main.classes = (function(mod, context){
+
+        var BaseExeption = function(message){
+            var msg = '[ERROR:' + context.projectName + '] ' + message;
+            this.getMessage = function(){ return msg; }
+        }
+
+        mod.NullOrUndefniedExeption = function(objName){
+            var base = new BaseExeption('The object "' + objName + '" is null or undefined.');
+            this.getMessage = function(){ return base.getMessage(); }
+        }
+
+        return mod;
+
+    })(main.classes || {}, main.dependancies.context)
+
+    // Modules here...
+
+})(window.main);
